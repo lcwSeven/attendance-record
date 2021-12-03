cd11e2c1b35666fd4403bb11664eec3936f355a0
liucaiwenseven@163.com <liucaiwenseven@163.com>
2021-12-03 22:43:08 +0800

    import:init

--- src/main/java/com/attendance/record/AttendanceRecordApplication.java
+++ src/main/java/com/attendance/record/AttendanceRecordApplication.java
@@ -1,16 +1,29 @@
 package com.attendance.record;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
 
+
+import java.awt.*;
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 /**
  * @author liucaiwen
@@ -19,16 +32,57 @@ import java.io.IOException;
 @SpringBootApplication
 public class AttendanceRecordApplication extends Application {
 
+
+    Desktop desktop = Desktop.getDesktop();
     private static ApplicationContext applicationContext;
 
     public static void main(String[] args) {
-        applicationContext = SpringApplication.run(AttendanceRecordApplication.class, args);
+
+        SpringApplicationBuilder builder = new SpringApplicationBuilder(AttendanceRecordApplication.class);
+        applicationContext = builder.headless(false).web(WebApplicationType.NONE).run(args);
         Application.launch(AttendanceRecordApplication.class, args);
 
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+
+//        FileChooser fileChooser = new FileChooser();
+//        Button openButton = new Button("点击选择文件");
+//        Button openMultipleButton = new Button("打开");
+//
+//        openButton.setOnAction(
+//                (final ActionEvent e) -> {
+//                    File file = fileChooser.showOpenDialog(primaryStage);
+//                    if (file != null) {
+//                        openFile(file);
+//                    }
+//                });
+//        openMultipleButton.setOnAction(
+//                (final ActionEvent e) -> {
+//                    List<File> list =
+//                            fileChooser.showOpenMultipleDialog(primaryStage);
+//                    if (list != null) {
+//                        list.stream().forEach((file) -> {
+//                            openFile(file);
+//                        });
+//                    }
+//                });
+//
+//        final GridPane inputGridPane = new GridPane();
+//
+//        GridPane.setConstraints(openButton, 0, 0);
+//        GridPane.setConstraints(openMultipleButton, 1, 0);
+//        inputGridPane.setHgap(6);
+//        inputGridPane.setVgap(6);
+//        inputGridPane.getChildren().addAll(openButton, openMultipleButton);
+//
+//        final Pane rootGroup = new VBox(12);
+//        rootGroup.getChildren().addAll(inputGridPane);
+//        rootGroup.setPadding(new Insets(12, 12, 12, 12));
+
+        primaryStage.setWidth(500);
+        primaryStage.setHeight(500);
         primaryStage.setTitle("考勤工具");
         primaryStage.setScene(new Scene(createRoot()));
         primaryStage.show();
@@ -41,4 +95,12 @@ public class AttendanceRecordApplication extends Application {
         loader.setControllerFactory(applicationContext::getBean);
         return loader.load();
     }
+
+    private void openFile(File file) {
+        EventQueue.invokeLater(() -> {
+
+            System.out.println(file.getName());
+        });
+    }
+
 }
--- src/main/java/com/attendance/record/controller/MainController.java
+++ src/main/java/com/attendance/record/controller/MainController.java
@@ -1,7 +1,18 @@
 package com.attendance.record.controller;
 
 
+
+import com.alibaba.excel.EasyExcel;
+import com.alibaba.excel.ExcelReader;
+import com.alibaba.excel.read.builder.ExcelReaderBuilder;
+import com.alibaba.excel.read.builder.ExcelReaderSheetBuilder;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 import org.springframework.stereotype.Component;
+
+import java.io.File;
 
 /**
  * @author liucaiwen
@@ -9,4 +20,30 @@ import org.springframework.stereotype.Component;
  */
 @Component
 public class MainController {
+
+
+    public Button selectFile;
+    public TextField textField;
+
+    public Stage stage;
+
+    public void initialize() {
+
+        FileChooser fileChooser = new FileChooser();
+
+        selectFile.setOnAction(e -> {
+
+            final File file = fileChooser.showOpenDialog(stage);
+            if (file != null){
+                textField.setText(file.getAbsolutePath());
+
+
+            }
+        });
+
+    }
+
+    private void readExcel(File file){
+        final ExcelReaderBuilder read = EasyExcel.read(file);
+    }
 }
